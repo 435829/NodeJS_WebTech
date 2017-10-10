2dfef2fcd2b2581a714bda55ccf9d70ff8c9b632
435829 <435829@student.saxion.nl>
2017-10-10 19:28:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var mongodbUri = "mongodb://Robinbolink:abcd12@ds163034.mlab.com:63034/robindb";
+var options = {
+    useMongoClient: true,
+    // socketTimeout: 0,
+    keepAlive: true,
+    reconnectTries: 30
+};
+
+//db.on db.once(hier alle code in), op internet kijken
+
+var db = mongoose.connect(mongodbUri, options);
+console.log("test");
+{
+// // respond with "hello world" when a GET request is made to the homepage
+
+    app.get('/', function (req, res) {
+        res.send('Hello World!')
+    });
+
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!')
+    });
+//
+//
+
+//Film object aangemaakt met verschillende variabelen
+    var film = new Schema({
+        imdb_number: Number,
+        titel: String,
+        datum: String,
+        lengte: Number,
+        regisseur: String,
+        beschrijving: String
+    });
+
+//User object aangemaakt met verschillende variabelen
+    var user = new Schema({
+        achternaam: String,
+        tussenvoegsels: String,
+        voornaam: String,
+        username: String,
+        wachtwoord: String
+    });
+
+// var rating = new Schema({
+//     sterren: Number,
+//     user: mongoose.Types.ObjectId(),
+//     film: mongoose.Types.ObjectId()
+// });
+
+    var Film = mongoose.model('Film', film);
+    var User = mongoose.model('User', user);
+// var Rating = mongoose.model('Rating', rating);
+
+// //Manier om methode aan te maken
+// film.statics.findByIMDB = function (imbd_number, cb) {
+//     return this.findByIMDB({imbd_number: new RegExp(imbd_number, 'i')}, cb);
+// };
+//
+// user.statics.findbyUsername = function (username, cb) {
+//     return this.findbyUsername({username: new RegExp(username, 'i')}, cb);
+// };
+
+    var newFilm = new Film({
+        imdb_nummer: 1234,
+        titel: 'test',
+        datum: '01-09-2017',
+        lengte: 20,
+        regisseur: 'Robin Bolink',
+        beschrijving: 'Leuke film'
+    });
+
+    newFilm.save();
+}
+
+
