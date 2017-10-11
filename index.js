0981c1b7a1d4bf58d72033508fd9c0dec9a8a348
435829 <435829@student.saxion.nl>
2017-10-11 12:11:52 +0200

    users en films toegevoegd

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ var options = {
     reconnectTries: 30
 };
 
+app.get('/api/users', function (req, res) {
+
+    console.log('show all users');
+});
+
 //db.on db.once(hier alle code in), op internet kijken
 
 var db = mongoose.connect(mongodbUri, options);
@@ -71,22 +76,145 @@ console.log("test");
     var Rating = mongoose.model('Rating', rating);
 
 
-    var newFilm = new Film({
-        imdb_nummer: 1234,
-        titel: 'test',
-        datum: '01-09-2017',
-        lengte: 20,
-        regisseur: 'Robin Bolink',
-        beschrijving: 'Leuke film'
-    });
+    // var newFilm = new Film({
+    //     imdb_nummer: 1234,
+    //     titel: 'test',
+    //     datum: '01-09-2017',
+    //     lengte: 20,
+    //     regisseur: 'Robin Bolink',
+    //     beschrijving: 'Leuke film'
+    // });
 
-    newFilm.save(function (err, result) {
-        if (err) {
-            return console.error(err);
-        } else {
-
-        }
-    });
+    // var newFilm2 = new Film({
+    //     imdb_nummer: 12,
+    //     titel: 'test123',
+    //     datum: '10-10-2017',
+    //     lengte: 10,
+    //     regisseur: 'Robert Storm',
+    //     beschrijving: 'Saaie film'
+    // });
+    // newFilm2.save();
+    //
+    // var newFilm3 = new Film({
+    //     imdb_nummer: 24,
+    //     titel: 'aardbij',
+    //     datum: '10-12-2017',
+    //     lengte: 1,
+    //     regisseur: 'Joran Storm',
+    //     beschrijving: 'film'
+    // });
+    // newFilm3.save();
+    //
+    // var newFilm4 = new Film({
+    //     imdb_nummer: 23,
+    //     titel: 'testje',
+    //     datum: '14-09-2017',
+    //     lengte: 2,
+    //     regisseur: 'David test',
+    //     beschrijving: 'piraten film'
+    // });
+    // newFilm4.save();
+    //
+    // var newFilm5 = new Film({
+    //     imdb_nummer: 45,
+    //     titel: 'tygui',
+    //     datum: '23-2-2017',
+    //     lengte: 56,
+    //     regisseur: 'David Storm',
+    //     beschrijving: 'Mooie film'
+    // });
+    // newFilm5.save();
+    //
+    //
+    // var newUser1 = new User({
+    //     achternaam: 'Storm',
+    //     tussenvoegsels: 'van',
+    //     voornaam: 'Robert',
+    //     username: 'RobertStorm',
+    //     wachtwoord: 'test12'
+    // });
+    // newUser1.save();
+    //
+    // var newUser2 = new User({
+    //     achternaam: 'Bolink',
+    //     tussenvoegsels: 'niks',
+    //     voornaam: 'Robin',
+    //     username: 'RobinBolink',
+    //     wachtwoord: 'test12123123'
+    // });
+    // newUser2.save();
+    //
+    // var newUser3 = new User({
+    //     achternaam: 'Bruijn',
+    //     tussenvoegsels: 'de',
+    //     voornaam: 'Joran',
+    //     username: 'JorandeBruijn',
+    //     wachtwoord: 'bruintje'
+    // });
+    // newUser3.save();
+    //
+    // var newUser4 = new User({
+    //     achternaam: 'Hollegien',
+    //     tussenvoegsels: 'niks',
+    //     voornaam: 'David',
+    //     username: 'DavidHollegien',
+    //     wachtwoord: 'david'
+    // });
+    // newUser4.save();
+    //
+    // var newUser5 = new User({
+    //     achternaam: 'Vincent',
+    //     tussenvoegsels: 'van',
+    //     voornaam: 'Overvelde',
+    //     username: 'vinnie',
+    //     wachtwoord: 'aardbei'
+    // });
+    // newUser5.save();
+    //
+    // var newUser6 = new User({
+    //     achternaam: 'Scholten',
+    //     tussenvoegsels: 'van',
+    //     voornaam: 'Tim',
+    //     username: 'TimScholten',
+    //     wachtwoord: 'scholt'
+    // });
+    // newUser6.save();
+    //
+    // var newUser7 = new User({
+    //     achternaam: 'aardbei',
+    //     tussenvoegsels: 'van',
+    //     voornaam: 'banaan',
+    //     username: 'banenen',
+    //     wachtwoord: 'aardbei'
+    // });
+    // newUser7.save();
+    //
+    // var newUser8 = new User({
+    //     achternaam: 'Tafel',
+    //     tussenvoegsels: 'van',
+    //     voornaam: 'Stoel',
+    //     username: 'TafelMetStoel',
+    //     wachtwoord: 'tafelstoel'
+    // });
+    // newUser8.save();
+    //
+    // var newUser9 = new User({
+    //     achternaam: 'Bord',
+    //     tussenvoegsels: 'van',
+    //     voornaam: 'appel',
+    //     username: 'appelBord',
+    //     wachtwoord: 'peer'
+    // });
+    // newUser9.save();
+    //
+    // var newUser10 = new User({
+    //     achternaam: 'laptop',
+    //     tussenvoegsels: 'van',
+    //     voornaam: 'pc',
+    //     username: 'pcLaptop',
+    //     wachtwoord: 'lappie'
+    // });
+    // newUser10.save();
 
 }
 
