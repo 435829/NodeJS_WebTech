568144ed221f29c528a562135d3d7da4edfcb5a2
RobertStorm <438924@student.saxion.nl>
2017-11-02 12:14:57 +0100

    tests and some incompletion fixes for tests

--- routing/authentication.js
+++ routing/authentication.js
@@ -17,25 +17,28 @@ var router = express.Router();
 // https://jwt.io/introduction/
 router.post('/authenticate/', function (req, res) {
     //Controleer of er een username ingevuld is
-    if (!req.body.username){
+    if (!req.body.username) {
         res.status(400).send('Username required');
         return;
     }
 //Controleer of er een wachtwoord is ingevuld
-    if (!req.body.password){
+    if (!req.body.password) {
         res.status(400).send('Password required');
         return;
     }
 
 //Vind de user die in wil logen door middel van de username
     User.findOne({username: req.body.username}, function (err, user) {
+        if (!user) {
+            res.status(404).send('User not found');
+        } else
         //Controleer het wachtwoord
-        if (user.checkPassword(user, req.body.password)){
+        if (user.checkPassword(user, req.body.password)) {
             //Maak een token aan met de username van de gebruiker
             var myToken = jwt.sign({username: req.body.username}, 'super-secret-key');
             res.status(200).json(myToken);
         } else {
-            res.status(401).send('Incorrect password');
+            res.status(400).send('Incorrect password');
         }
     });
 });
